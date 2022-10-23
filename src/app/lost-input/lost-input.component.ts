d8447a9b4fdc147c928d1143d20a377bb95087b6
yamamoto-yuka <Yuka.Yamamoto@vanarts.com>
2022-10-23 13:25:50 -0700

    Created an inpiut form

--- src/app/lost-input/lost-input.component.ts
+++ src/app/lost-input/lost-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../services/common.service';
 
 @Component({
   selector: 'app-lost-input',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lost-input.component.scss']
 })
 export class LostInputComponent implements OnInit {
+  itemName='';
+  description ='';
+  image='';
+  date='';
+  location='';
+  name='';
+  email='';
+  phoneNumber='';
+  formMessageHidden = true;
+  constructor(private cs:CommonService) { }
 
-  constructor() { }
+  submit(itemName:any, location:any, email:any, phoneNumber:any,  description:any,  image:any, date:any,  name:any){
+    this.cs.addPost( this.itemName, this.location, this.email, this.phoneNumber, this.description,this.image, this.date, this.name).subscribe(data =>{
+      this.formMessageHidden = false;
+      console.log(data);
+    })
+  }
 
   ngOnInit(): void {
   }
