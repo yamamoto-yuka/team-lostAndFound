d45dd0147937a1fadd1b87e682c01529bebef85b
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-11-08 19:22:24 -0800

    made some changes

--- src/app/lost-input/lost-input.component.ts
+++ src/app/lost-input/lost-input.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../services/common.service';
+import{Router} from '@angular/router'
+// import { format } from 'mysql2';
+
 
 @Component({
   selector: 'app-lost-input',
@@ -7,23 +10,87 @@ import { CommonService } from '../services/common.service';
   styleUrls: ['./lost-input.component.scss']
 })
 export class LostInputComponent implements OnInit {
-  itemName='';
-  description ='';
+  title='';
   image='';
-  date='';
   location='';
-  name='';
+  description='';
+  identifying_question='';
+  claimed=false;
+  contact_name='';
   email='';
-  phoneNumber='';
-  formMessageHidden = true;
-  constructor(private cs:CommonService) { }
+  phonenumber='';
+  date_found='';
+  filename:string= '';
 
-  submit(itemName:any, location:any, email:any, phoneNumber:any,  description:any,  image:any, date:any,  name:any){
-    this.cs.addPost( this.itemName, this.location, this.email, this.phoneNumber, this.description,this.image, this.date, this.name).subscribe(data =>{
-      this.formMessageHidden = false;
-      console.log(data);
-    })
+  myformdata:any;
+
+  constructor(private cs:CommonService,  private router:Router) { }
+
+
+
+  trackFile(event:any){
+    console.log(event);
+    let myfile = event.target.files[0];
+    this.filename = myfile.name;
+    // it is an array so you need to target which index you want //
+    console.log(myfile);
+
+    // now send the file details to a server with formdata format
+    const formdata = new FormData();
+    // FormData is predefied method
+    formdata.append("image", myfile, myfile.name);
+    this.myformdata = formdata
+    console.log("form data is ", myfile)
+
+
+
+
   }
+
+
+  submit (){
+
+
+
+    // console.log(this.title, this.filename, this.location, this.description,this.identifying_question, this.claimed, this.contact_name, this.email, this.phonenumber, this.date_found,this.myformdata);
+
+
+    this.cs.uploadFile(this.myformdata).subscribe(uplodMessage=>{
+      console.log(uplodMessage);
+      console.log("uplodMessage is ", uplodMessage)
+
+    })
+
+    this.cs.addPost( this.title, this.filename, this.location,this.description, this.identifying_question, this.claimed, this.contact_name, this.email, this.phonenumber, this.date_found).subscribe(data =>{
+
+     console.log("addPost data is ", data)
+
+      // this.formMessageHidden = false;
+      // console.log("data " , data);
+
+      this.router.navigate(['/']);
+    })
+
+
+    window.location.reload();
+    window.scrollTo(0, 0);
+
+    // form.reset();
+
+
+
+    //   this.location ='';
+    //   this.description ='';
+    //   this.identifying_question='';
+    //   this.contact_name='';
+    //   this.email='';
+    //   this.phonenumber='';
+
+
+  }
+
+
+
 
   ngOnInit(): void {
   }
